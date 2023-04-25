48f30f3fe724d574d8f7ffdf0159d538b31aa16d
Valentina Hernández <vahernandezmo@unal.edu.co>
2023-04-24 22:25:36 -0500

    Removed filter applications

--- Proyecto/Código base/color.py
+++ Proyecto/Código base/color.py
@@ -4,13 +4,6 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-def getSobel(img):
-    
-    #Sobel: cv2.Sobel(src, ddepth, dx, dy, ksize)
-    sobelx = (cv.Sobel(img,-1,1,0,ksize=3))
-    sobely = (cv.Sobel(img,-1,0,1,ksize=3))
-    return sobelx+sobely
-
 camera = Camera()
 device = camera.createColorCamera(previewSize=(600,600))
 
@@ -33,8 +26,6 @@ while True:
         
     # Display output image
     if imOut is not None:
-        img_grayscale = cv.cvtColor(imOut, cv.COLOR_RGB2GRAY)
-        filtered_img = getSobel(img_grayscale)
         cv.imshow("Color Camera", imOut)
         cv.imwrite(f"./images/{images}.jpg", imOut)
         images+=1
