e89eb333903a211e250308e60fedaaf2d267a26d
Valentina Hernández <vahernandezmo@unal.edu.co>
2023-04-24 22:19:06 -0500

    Added a filter script

--- Proyecto/Código base/filters.py
+++ Proyecto/Código base/filters.py
@@ -21,12 +21,12 @@ q_rgb = device.getOutputQueue(name="rgb", maxSize=1)
 cv.namedWindow("Color Camera")
 
 images = 0
-
+sobel = False
 while True:
     imOut = None
     # Get rgb frame
     in_rgb = q_rgb.tryGet()
-        
+    
     if in_rgb is not None:
         
         imOut = camera.getFrame(q_rgb)
@@ -34,12 +34,19 @@ while True:
     # Display output image
     if imOut is not None:
         img_grayscale = cv.cvtColor(imOut, cv.COLOR_RGB2GRAY)
-        filtered_img = getSobel(img_grayscale)
-        cv.imshow("Color Camera", imOut)
-        cv.imwrite(f"./images/{images}.jpg", imOut)
+
+        if sobel:
+            filtered_img = getSobel(img_grayscale)
+        else: 
+            ret, filtered_img = cv.threshold(img_grayscale, 128, 255, cv.THRESH_BINARY)
+
+        cv.imshow("Color Camera", filtered_img)
+        cv.imwrite(f"./images/{images}.jpg", filtered_img)
         images+=1
     # Check for keyboard input
     key = cv.waitKey(1)
     if key == ord('q'):
         # Quit when q is pressed
         break
+    elif key == ord('t'):
+        sobel = not sobel
